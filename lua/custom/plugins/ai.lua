b1f8dd1a7093ec9dc906bd3a43109375d282b577
whysosergious <zergski@gmail.com>
2026-01-08 14:01:43 +0200

    tried adding free ai completion and chat for ollama... inline works questionably

---
+++ lua/custom/plugins/ai.lua
@@ -0,0 +1,30 @@
+return {
+
+  {
+    'Jacob411/Ollama-Copilot',
+    event = 'InsertEnter',
+    opts = {
+      ollama_model_opts = { model = 'starcoder2:3b', num_predict = 80 },
+      keymaps = {
+        suggestion = '<leader>os',
+        insert_accept = '<Tab>',
+      },
+    },
+  },
+
+  {
+    'olimorris/codecompanion.nvim', -- Fixed repo name
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = 'ollama',
+          opts = { model = 'starcoder2:3b' },
+        },
+      },
+    },
+    keys = {
+      { '<leader>oc', '<cmd>CodeCompanionChat<cr>', desc = 'Ollama Chat' },
+    },
+  },
+}
