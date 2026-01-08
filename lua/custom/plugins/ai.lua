4b00eeb817147e19f413e61f15f75664b86130a9
whysosergious <zergski@gmail.com>
2026-01-08 19:28:41 +0200

    added neotest

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -5,7 +5,7 @@ return {
     opts = {
       ollama_model_opts = { model = 'starcoder2:3b', num_predict = 80 },
       keymaps = {
-        suggestion = '<leader>as',
+        suggestion = '<leader>os',
         insert_accept = '<Tab>',
       },
     },
@@ -23,11 +23,8 @@ return {
       },
     },
     keys = {
-      { '<leader>ac', '<cmd>CodeCompanionChat<cr>', desc = 'AI Chat' },
-      { '<leader>ag', '<cmd>CodeCompanionGenerate<cr>', mode = 'v', desc = 'AI Generate' },
-      { '<leader>ar', '<cmd>CodeCompanionRefactor<cr>', mode = 'v', desc = 'AI Refactor' },
-      { '<leader>ae', '<cmd>CodeCompanionExplain<cr>', mode = 'v', desc = 'AI Explain' },
-      { '<leader>at', '<cmd>CodeCompanionAddTests<cr>', mode = 'v', desc = 'AI Add Tests' },
+      { '<leader>oc', '<cmd>CodeCompanionChat<cr>', desc = '[c]hat with ollama' },
     },
   },
 }
+
---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-neotest/neotest-jest',
+      'marilari88/neotest-vitest',
+      'arthur944/neotest-bun',
+    },
+    keys = {
+      { '<leader>tr', '<cmd>Neotest run<cr>', desc = '[t]est [r]un' },
+      { '<leader>ti', '<cmd>Neotest output<cr>', desc = '[t]est [i]nfo' },
+      { '<leader>ts', '<cmd>Neotest summary<cr>', desc = '[t]est [s]ummary' },
+      { '<leader>ta', "<cmd>lua require('neotest').run.run({suite = true })<cr>", desc = '[t]est [a]ll' },
+    },
+    config = function()
+      require('neotest').setup {
+        settings = {
+          watch = true,
+        },
+        adapters = {
+          require 'neotest-jest',
+          require 'neotest-vitest',
+          require 'neotest-bun',
+        },
+      }
+    end,
+  },
+}
